311f1baccfe4df1a2ca0954f2f793857772b2667
Poorna-Vaishnavi <131003457+Poorna-Vaishnavi@users.noreply.github.com>
2023-07-19 21:37:25 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,15 @@
+def pow_num(a,b,c):
+    
+    if b==0:
+        return 1
+    if b==1:
+        return a
+    p=pow_num(a,b//2,c) 
+    if b%2==0:
+        return (p*p)%c
+    else:
+        return (p*p*a)%c
+
+a,b,c=map(int,input().split())
+print(pow_num(a,b,c))        
+
